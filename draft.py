87f1763ff7eb22ea4490338b75d836f4bb290c0b
HellOwhatAs <xjq701229@outlook.com>
2023-05-19 22:13:41 +0800

    完成图的构建（包括点和边的feature）

--- draft.py
+++ draft.py
@@ -2,6 +2,15 @@ import json, dgl, torch, os
 
 os.environ["DGLBACKEND"] = "pytorch"
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def ca2vec(author_info_covid):
+    ret = torch.zeros((len(author_info_covid), max_field + 1), dtype=torch.int64)
+    for i, elem in sorted(author_info_covid.items()):
+        for j, k in elem['ca']:
+            ret[i, j] = k
+    return ret
+
 if not os.path.exists('./tmp.bin'):
     with open('./archive/author_info_covid.json', 'r') as f:
         author_info_covid = {int(k): {k1: (v1 if k1 == "ct" else [[int(i0) - 1, i1] for i0, i1 in v1]) for k1, v1 in v.items()} for k, v in json.load(f).items()}
@@ -9,9 +18,22 @@ if not os.path.exists('./tmp.bin'):
         authors_to_pred = torch.tensor([int(i) for i in f.read().strip().split('\n')[1:]], dtype=torch.int64)
     with open('./archive/coauthor_covid.txt', 'r') as f:
         coauthor_covid = torch.tensor([[int(j) for j in i.split()] for i in f.read().strip().split('\n')], dtype=torch.int64)
-    torch.save((author_info_covid, authors_to_pred, coauthor_covid), "tmp.bin")
-else:
-    author_info_covid, authors_to_pred, coauthor_covid = torch.load("tmp.bin")
 
-g = dgl.graph((coauthor_covid[:, 0], coauthor_covid[:, 1]), num_nodes = len(author_info_covid))
+    idx2ct = sorted({v['ct'] for k, v in author_info_covid.items()})
+    ct2idx = {elem: i for i, elem in enumerate(idx2ct)}
+    max_field = max(max(i[0] for i in v['ca']) for k, v in author_info_covid.items())
+    ct = torch.tensor([ct2idx[v['ct']] for k, v in sorted(author_info_covid.items())], dtype=torch.int64)
+    ca = ca2vec(author_info_covid)
+
+    torch.save((authors_to_pred, coauthor_covid, idx2ct, ct2idx, max_field, ct, ca), "tmp.bin")
+else:
+    authors_to_pred, coauthor_covid, idx2ct, ct2idx, max_field, ct, ca = torch.load("tmp.bin")
+
+
+coauthor_covid, ct, ca = coauthor_covid.to(device), ct.to(device), ca.to(device)
+g = dgl.graph((coauthor_covid[:, 0], coauthor_covid[:, 1]), num_nodes = len(ct), device=device)
+g.ndata['ct'] = ct
+g.ndata['ca'] = ca
+g.edata['t'] = coauthor_covid[:, 2]
+
 print(g)
