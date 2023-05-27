3d361cd7097629c6592715c29511727a07d4f2b0
HarryXu <harry_xu@sjtu.edu.cn>
2023-05-27 15:34:52 +0800

    weighted graph

--- get_features.py
+++ get_features.py
@@ -18,20 +18,22 @@ def normal(dic):
 
 def get_features():
     G = nx.Graph()
+    G0 = nx.Graph()
     G.add_nodes_from([i for i in range(1275456)])
     # print(G.nodes())
     # print(coauthor_covid[:, :2].tolist()[:10])
-    G.add_edges_from(coauthor_covid[:, :2].tolist())
+    G0.add_edges_from(coauthor_covid[:, :2].tolist())
+    G.add_weighted_edges_from(coauthor_covid.tolist())
     print('construct graph ok.')
 
     # print(nx.is_connected(G))
-    G.remove_nodes_from(nx.isolates(G.copy()))
+    # G.remove_nodes_from(nx.isolates(G.copy()))
     # print(nx.is_connected(G))
 
     k = 10
-    DEG = nx.degree_centrality(G)
+    DEG = nx.degree_centrality(G0)
     DEG = normal(DEG)
-    BC = nx.betweenness_centrality(G, k) 
+    BC = nx.betweenness_centrality(G0, k) 
     BC = normal(BC)
     EBC = nx.edge_betweenness_centrality(G, k, weight='weight')  # 边的特征
     EBC = normal(EBC)
