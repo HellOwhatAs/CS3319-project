a347fe12ef7f5b59d13fa6f954e9ca2419436f40
HellOwhatAs <xjq701229@outlook.com>
2023-05-29 14:29:00 +0800

    commit

--- model_def.py
+++ model_def.py
@@ -1,7 +1,6 @@
 import dgl.nn.pytorch as dglnn
 import torch.nn as nn
 import torch.nn.functional as F
-import torch, copy
 
 class GNN(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_layers = 2,
@@ -36,71 +35,3 @@ class GNN(nn.Module):
         feat = self.convs[-1](graph, feat, edge_weight = eweight)
 
         return feat if self.return_embeds else self.softmax(feat)
-    
-
-
-def train(model, g, optimizer, loss_fn, max_epoch):
-    loss = 0
-    best_val_acc = 0
-    
-    features = g.ndata["feat"]
-    labels = g.ndata["label"]
-    train_mask = g.ndata["train_mask"]
-    val_mask = g.ndata["val_mask"]
-    test_mask = g.ndata["test_mask"]
-    
-    for e in range(max_epoch):
-        
-        output = model(g, features, edge_weight = g.edata['t'])
-        loss = loss_fn(output[train_mask], labels[train_mask])
-        val_acc = torch.sum(output[val_mask].max(1)[1] == labels[val_mask]) / torch.sum(val_mask)
-        
-        if best_val_acc < val_acc:
-            best_val_acc = val_acc
-            best_model = copy.deepcopy(model)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if e % 5 == 0:
-            print(
-                "In epoch {}, loss: {:.3f}, val acc: {:.3f} (best {:.3f})".format(
-                    e, loss, val_acc, best_val_acc
-                )
-            )
-    return best_model, best_val_acc
-
-@torch.no_grad()
-def test(model, g):
-    model.eval()
-    features = g.ndata["feat"]
-    labels = g.ndata["label"]
-    train_mask = g.ndata["train_mask"]
-    val_mask = g.ndata["val_mask"]
-    test_mask = g.ndata["test_mask"]
-
-    out = model(g, features)
-    
-    test_acc = torch.sum(out[test_mask].max(1)[1] == labels[test_mask]) / torch.sum(test_mask)
-
-    return test_acc
-
-if __name__ == '__main__':
-    from draft import g, device
-    model = GNN(
-        input_dim = g.ndata["feat"].shape[1],
-        hidden_dim = 128,
-        output_dim = 10,                            # TODO: output 什么？
-        num_layers = 2,
-        dropout = 0.5
-    ).to(device)
-    model.reset_parameters()
-    
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    loss_fn = F.nll_loss
-
-    best_model, best_valid_acc = train(model, g, optimizer, loss_fn, 100)
-    
-    test_acc = test(best_model, g)
-    print(f'Test: {100 * test_acc:.2f}%')
