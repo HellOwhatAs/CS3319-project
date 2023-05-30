b0974ca0eb7367d0327a3a6f9dbad6e76b211ac5
HellOwhatAs <xjq701229@outlook.com>
2023-05-30 20:13:38 +0800

    edge_mask of sg

--- gnnexplainer.py
+++ gnnexplainer.py
@@ -6,18 +6,20 @@ from tqdm import tqdm
 from get_features import classes as cluster_label
 from mycsv import csv
 
-def calc_sgmask(g, sg, k2idx):
+def calc_sgmask(g, sg, edge_mask, k2idx):
     node_sg = ['0'] * g.num_nodes()
     for i in sg.ndata['ori_id'].cpu().numpy():
         node_sg[i] = '1'
 
     edge_sg = ['0'] * (g.num_edges() // 2)
-    for s, t in zip(*sg.edges()):
+    edge_weight = [0.] * (g.num_edges() // 2)
+    for s, t, w in tqdm(zip(*sg.edges(), edge_mask), desc='Edge mask calculate', total=sg.num_edges()):
         s, t = sg.ndata['ori_id'][s].item(), sg.ndata['ori_id'][t].item()
-        k = f'{min(s, t)},{max(s, t)}'
-        edge_sg[k2idx[k]] = '1'
+        idx = k2idx[f'{min(s, t)},{max(s, t)}']
+        edge_sg[idx] = '1'
+        edge_weight[idx] += w.item()
 
-    return node_sg, edge_sg
+    return node_sg, edge_sg, [str(i) for i in edge_weight]
 
 if __name__ == '__main__':
 
@@ -35,7 +37,7 @@ if __name__ == '__main__':
     model = Model(features.shape[1], 128, 3).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
-    with tqdm(range(2000)) as tbar:
+    with tqdm(range(2000), desc='GNN training') as tbar:
         for epoch in tbar:
             logits = model(g, features)
             loss = criterion(logits[train_mask], labels[train_mask])
@@ -53,17 +55,17 @@ if __name__ == '__main__':
     st = [f'{min(int(s), int(t))},{max(int(s), int(t))}' for s, t in zip(edge_csv['source'], edge_csv['target'])]
     k2idx = {elem: idx for idx, elem in enumerate(st)}
 
-    explainer = GNNExplainer(model, num_hops=1)
+    explainer = GNNExplainer(model, num_hops=2)
     new_center, sg, feat_mask, edge_mask = explainer.explain_node(old2new[36291], g, features) # 36291: 桥梁节点; 528886: 中心节点; 7976: 边缘节点
-    node_csv['node_sg_bridge'], edge_csv['edge_sg_bridge'] = calc_sgmask(g, sg, k2idx)
+    node_csv['node_sg_bridge'], edge_csv['edge_sg_bridge'], edge_csv['edge_sg_bridge_mask'] = calc_sgmask(g, sg, edge_mask, k2idx)
 
-    explainer = GNNExplainer(model, num_hops=1)
+    explainer = GNNExplainer(model, num_hops=2)
     new_center, sg, feat_mask, edge_mask = explainer.explain_node(old2new[528886], g, features)
-    node_csv['node_sg_center'], edge_csv['edge_sg_center'] = calc_sgmask(g, sg, k2idx)
+    node_csv['node_sg_center'], edge_csv['edge_sg_center'], edge_csv['edge_sg_center_mask'] = calc_sgmask(g, sg, edge_mask, k2idx)
 
-    explainer = GNNExplainer(model, num_hops=1)
+    explainer = GNNExplainer(model, num_hops=2)
     new_center, sg, feat_mask, edge_mask = explainer.explain_node(old2new[7976], g, features)
-    node_csv['node_sg_margin'], edge_csv['edge_sg_margin'] = calc_sgmask(g, sg, k2idx)
+    node_csv['node_sg_margin'], edge_csv['edge_sg_margin'], edge_csv['edge_sg_margin_mask'] = calc_sgmask(g, sg, edge_mask, k2idx)
 
     node_csv.write('node_sg.csv')
     edge_csv.write('edge_sg.csv')
